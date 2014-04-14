8030a0dfaead122dab22d40f26859c8095c11803
viny971 <viny971@gmail.com>
2014-04-14 23:58:55 +0200

    Première mise en ligne du projet Calder

---
+++ calder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.2
+
+from math import *
+from tkinter import *
+
+
+def dessineLigne(x1,x2,x3,x4,y,p1,p2,dessin):
+	
+	X1 = x1 + (x2-x1)/2
+	X2 = x3 + (x4-x3)/2
+	l1 = (p2*(X2-X1))/(p1+p2)
+	X = X1 + l1
+	print("X1 = ",X1,"X2 = ",X2,"l1 = ",l1,"X = ",X)
+	dessin.create_line(X1,y,X1,y-50)
+	dessin.create_line(X2,y,X2,y-50)
+	dessin.create_line(X1,y-50,X2,y-50)
+	dessin.create_line(X,y-50,X,y-100)
+
+def dessineCercle(x1,x2,y,p1,p2,dessin):
+
+	x3 = x1 + 2*sqrt(p1)
+	x4 = x2 + 2*sqrt(p2)
+	y3 = y + 2*sqrt(p1)
+	y4 = y + 2*sqrt(p2)
+	print("x3 = ",x3,"x4 = ",x4,"y3 = ",y+x3,"y4 = ",y+x4)
+	dessin.create_oval(x1,y,x3,y3,fill='#809FFF')
+	dessin.create_oval(x2,y,x4,y4,fill='#FF6347')
+	dessineLigne(x1,x3,x2,x4,y,p1,p2,dessin)
+
+fenetre = Tk()
+
+dessin = Canvas(fenetre, width =500, height=500)
+
+dessin.pack()
+#dessin.create_oval(50,100,150,200,fill='blue')
+#dessin.create_oval(250,100,350,200,fill='red')
+dessineCercle(50,250,100,500,2000,dessin)
+#dessineLigne(50,150,250,350,100,200,200,dessin)
+
+fenetre.mainloop()
+	
+	
