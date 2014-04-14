b438f864b31a115c65fa2d78c3f242951fff1b4a
viny971 <viny971@gmail.com>
2014-04-15 01:16:02 +0200

    Couleurs aléatoires

--- calder.py
+++ calder.py
@@ -2,15 +2,15 @@
 
 from math import *
 from tkinter import *
-
-
+from couleur import *
+from random import *
 def dessineLigne(x1,x2,x3,x4,y,p1,p2,dessin):
 	
 	X1 = x1 + (x2-x1)/2
 	X2 = x3 + (x4-x3)/2
 	l1 = (p2*(X2-X1))/(p1+p2)
 	X = X1 + l1
-	print("X1 = ",X1,"X2 = ",X2,"l1 = ",l1,"X = ",X)
+	#print("X1 = ",X1,"X2 = ",X2,"l1 = ",l1,"X = ",X)
 	dessin.create_line(X1,y,X1,y-50)
 	dessin.create_line(X2,y,X2,y-50)
 	dessin.create_line(X1,y-50,X2,y-50)
@@ -22,9 +22,11 @@ def dessineCercle(x1,x2,y,p1,p2,dessin):
 	x4 = x2 + 2*sqrt(p2)
 	y3 = y + 2*sqrt(p1)
 	y4 = y + 2*sqrt(p2)
-	print("x3 = ",x3,"x4 = ",x4,"y3 = ",y+x3,"y4 = ",y+x4)
-	dessin.create_oval(x1,y,x3,y3,fill='#809FFF')
-	dessin.create_oval(x2,y,x4,y4,fill='#FF6347')
+	i = randrange(len(colors))
+	j = randrange(len(colors))
+	print("colors[",i,"] = ",colors[i],"colors[",j,"] = ",colors[j])
+	dessin.create_oval(x1,y,x3,y3,fill=colors[i])
+	dessin.create_oval(x2,y,x4,y4,fill=colors[j])
 	dessineLigne(x1,x3,x2,x4,y,p1,p2,dessin)
 
 fenetre = Tk()
